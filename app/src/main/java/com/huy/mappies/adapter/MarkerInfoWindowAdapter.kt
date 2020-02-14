8c86c28e5aebdd172ba6e0d7c9a33548c3bff247
Huy Bui <huythdnbkltv@gmail.com>
2020-02-14 09:22:14 +0200

    Add data binding to bookmark_layout.xml

--- app/src/main/java/com/huy/mappies/adapter/MarkerInfoWindowAdapter.kt
+++ app/src/main/java/com/huy/mappies/adapter/MarkerInfoWindowAdapter.kt
@@ -2,22 +2,27 @@ package com.huy.mappies.adapter
 
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.Marker
 import com.huy.mappies.R
+import com.huy.mappies.databinding.BookmarkLayoutBinding
 import com.huy.mappies.model.BookmarkView
 import com.huy.mappies.model.PlaceInfo
 
-class MarkerInfoWindowAdapter private constructor(private val markerView: View)
+class MarkerInfoWindowAdapter private constructor(private val binding: BookmarkLayoutBinding)
     : GoogleMap.InfoWindowAdapter
 {
 
     companion object {
         fun from(inflater: LayoutInflater): MarkerInfoWindowAdapter {
-            val view = inflater.inflate(R.layout.marker_layout, null)
-            return MarkerInfoWindowAdapter(view)
+            val binding: BookmarkLayoutBinding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.bookmark_layout,
+                null,
+                false
+            )
+            return MarkerInfoWindowAdapter(binding)
         }
     }
 
@@ -28,31 +33,29 @@ class MarkerInfoWindowAdapter private constructor(private val markerView: View)
 
     override fun getInfoContents(marker: Marker?): View {
 
-        val titleTextView = markerView.findViewById<TextView>(R.id.marker_title)
-        titleTextView.text = marker?.title.toString()
-
-        val subtitleTextView = markerView.findViewById<TextView>(R.id.marker_subtitle)
-        subtitleTextView.text = marker?.snippet ?: ""
+        with(binding) {
+            bookmarkTitle.text = marker?.title
+            bookmarkSubtitle?.text = marker?.snippet
+        }
 
         displayImage(marker)
 
-        return markerView
+        return binding.root
+
     }
 
     private fun displayImage(marker: Marker?) {
-        val imageView = markerView.findViewById<ImageView>(R.id.marker_image)
-
         when(marker?.tag) {
 
             is PlaceInfo -> {
                 val placeInfo: PlaceInfo? = marker.tag as PlaceInfo
-                imageView.setImageBitmap(placeInfo?.image)
+                binding.bookmarkImage.setImageBitmap(placeInfo?.image)
             }
 
             is BookmarkView -> {
                 val bookmarkView = marker.tag as BookmarkView
-                val bitmap = bookmarkView.getImage(markerView.context)
-                imageView.setImageBitmap(bitmap)
+                val bitmap = bookmarkView.getImage(binding.root.context)
+                binding.bookmarkImage.setImageBitmap(bitmap)
             }
 
         }
