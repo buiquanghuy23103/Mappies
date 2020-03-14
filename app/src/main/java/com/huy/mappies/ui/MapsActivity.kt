9a07470b7a0db51be7bff3f353ccd1691b2cf08d
Huy Bui <huythdnbkltv@gmail.com>
2020-03-14 13:31:48 +0200

    Gather map-related code to MapManager (single responsibility code)

--- app/src/main/java/com/huy/mappies/ui/MapsActivity.kt
+++ app/src/main/java/com/huy/mappies/ui/MapsActivity.kt
@@ -4,9 +4,7 @@ import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
 import android.os.Bundle
-import android.util.LongSparseArray
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -15,17 +13,11 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException
 import com.google.android.gms.common.GooglePlayServicesRepairableException
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.*
+import com.google.android.gms.maps.model.Marker
 import com.google.android.libraries.places.api.Places
-import com.google.android.libraries.places.api.model.Place
-import com.google.android.libraries.places.api.model.RectangularBounds
 import com.google.android.libraries.places.api.net.PlacesClient
 import com.google.android.libraries.places.widget.Autocomplete
 import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
@@ -34,7 +26,8 @@ import com.huy.mappies.adapter.BookmarkInfoWindowAdapter
 import com.huy.mappies.adapter.DrawerItemListAdapter
 import com.huy.mappies.model.BookmarkView
 import com.huy.mappies.model.PlaceInfo
-import com.huy.mappies.utils.*
+import com.huy.mappies.utils.getAppInjector
+import com.huy.mappies.utils.placeFields
 import com.huy.mappies.viewmodel.MapsViewModel
 import kotlinx.android.synthetic.main.activity_maps.*
 import kotlinx.android.synthetic.main.maps_drawer_view.*
@@ -53,11 +46,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, DrawerItemListAdap
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
-    private lateinit var map: GoogleMap
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var placesClient: PlacesClient
     private lateinit var viewModel: MapsViewModel
-    private val markers = LongSparseArray<Marker>()
+    private lateinit var mapManager: MapManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -67,17 +58,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, DrawerItemListAdap
         setupToolbar()
         setupDrawerToggleIcon()
         setupMapView()
-        setupLocationClient()
         setupPlacesClient()
         setupDrawer()
         setupSearchButton()
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        map = googleMap
-        setupMapsListener()
+        val infoWindowAdapter = BookmarkInfoWindowAdapter.from(layoutInflater)
+        mapManager = MapManager(googleMap, this, infoWindowAdapter)
+        mapManager.setupInfoWindowClickListener {
+            handleInfoWindowClick(it)
+        }
         observeBookmarkViews()
-        getCurrentLocation()
     }
 
     private fun setupViewModel() {
@@ -120,12 +112,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, DrawerItemListAdap
         mapFragment.getMapAsync(this)
     }
 
-
-
-    private fun setupLocationClient() {
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-    }
-
     private fun setupPlacesClient() {
         val apiKey = getString(R.string.google_maps_key)
         Places.initialize(this, apiKey)
@@ -135,11 +121,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, DrawerItemListAdap
     private fun observeBookmarkViews() {
 
         viewModel.allBookmarkViews.observe(this, Observer {
-            markers.clear()
-            map.clear()
-            it?.let {
-                it.forEach(this::addPlaceMarker)
-            }
+            mapManager.displayBookmarkViews(it)
         })
 
     }
@@ -148,76 +130,19 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, DrawerItemListAdap
 
         maps_drawer_layout.closeDrawer(drawerView)
 
-        val marker = bookmarkView.id?.let { markers[it] }
-        marker?.showInfoWindow()
-
-        zoomLocation(bookmarkView.latitude, bookmarkView.longtitude)
+        mapManager.zoomBookmarkView(bookmarkView)
 
     }
 
-    private fun addPlaceMarker(bookmarkView: BookmarkView) {
-        val iconResId = categoryToIconMap[bookmarkView.category]
-        val icon = iconResId?.let { BitmapDescriptorFactory.fromResource(iconResId) }
-
-        val markerOptions = MarkerOptions()
-            .position(LatLng(bookmarkView.latitude, bookmarkView.longtitude))
-            .title(bookmarkView.name)
-            .snippet(bookmarkView.phone)
-            .icon(icon)
-            .alpha(0.8f)
-
-        val marker = map.addMarker(markerOptions)
-        marker.tag = bookmarkView
-
-        bookmarkView.id?.let {
-            markers.append(it, marker)
-        }
-
-    }
 
     private fun getCurrentLocation() {
 
-        if (locationPermissionNotGranted()) {
-
+        mapManager.getCurrentLocation(locationPermissionNotGranted()){
             requestLocationPermissions()
-
-        } else {
-
-            map.isMyLocationEnabled = true
-            zoomInToCurrentLocation()
-
         }
 
     }
 
-    private fun zoomInToCurrentLocation() {
-        fusedLocationClient.lastLocation
-            .addOnCompleteListener {
-
-                val location = it.result
-
-                if (location != null) {
-
-                    val latLng = LatLng(location.latitude, location.longitude)
-                    val markerOptions = MarkerOptions().position(latLng)
-                        .title("You are here")
-
-                    map.clear() // Remove previous markers
-                    map.addMarker(markerOptions)
-                    zoomLocation(location.latitude, location.longitude)
-
-                } else {
-                    Timber.e("No location found")
-                }
-
-            }
-    }
-
-    private fun zoomLocation(latitude: Double, longtitude: Double) {
-        val latLng = LatLng(latitude, longtitude)
-        val cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 16.0f)
-        map.animateCamera(cameraUpdate)
-    }
 
     private fun locationPermissionNotGranted(): Boolean {
         val permission = ActivityCompat.checkSelfPermission(this,
@@ -248,83 +173,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, DrawerItemListAdap
         }
     }
 
-    private fun setupMapsListener() {
-        setupInfoWindow()
-        map.setOnPoiClickListener {
-            handlePoiClick(it)
-        }
-        map.setOnInfoWindowClickListener {
-            handleInfoWindowClick(it)
-        }
-    }
-
-    private fun setupInfoWindow() {
-        val infoWindowAdapter = BookmarkInfoWindowAdapter.from(layoutInflater)
-        map.setInfoWindowAdapter(infoWindowAdapter)
-    }
-
-    private fun handlePoiClick(pointOfInterest: PointOfInterest) {
-        val placeRequest = buildPlaceRequest(pointOfInterest)
-        placesClient.fetchPlace(placeRequest)
-            .addOnSuccessListener { response ->
-                val place = response.place
-                displayPoiDetails(place)
-            }
-            .addOnFailureListener {error ->
-                if (error is ApiException) {
-                    Timber.e("Place not found: ${error.message}, statusCode=${error.statusCode}")
-                }
-            }
-
-    }
-
-    private fun displayPoiDetails(place: Place) {
-
-        val maxWidth = resources.getDimensionPixelSize(R.dimen.default_image_width)
-        val maxHeight = resources.getDimensionPixelSize(R.dimen.default_image_height)
-        val photoRequest = buildPhotoRequest(place, maxWidth, maxHeight)
-
-        if (photoRequest == null) {
-            addPlaceMarker(place, null)
-            return
-        } else {
-
-            placesClient.fetchPhoto(photoRequest)
-                .addOnSuccessListener { response ->
-                    val bitmap = response.bitmap
-                    addPlaceMarker(place, bitmap)
-                }
-                .addOnFailureListener {error ->
-                    if (error is ApiException) {
-                        Timber.e("Place photo not found: ${error.message}, statusCode=${error.statusCode}")
-                    } else {
-                        Timber.e(error)
-                    }
-                }
-
-        }
-
-    }
-
-    private fun addPlaceMarker(place: Place, bitmap: Bitmap?) {
-
-        val defaultMarkerIcon = BitmapDescriptorFactory.defaultMarker(
-            BitmapDescriptorFactory.HUE_AZURE
-        )
-
-        val markerOptions = MarkerOptions()
-            .position(place.latLng ?: LatLng(42.90237, -78.8704978))
-            .title(place.name)
-            .snippet(place.phoneNumber)
-            .icon(defaultMarkerIcon)
-            .alpha(0.08f)
-
-        val marker = map.addMarker(markerOptions)
-        marker.tag = PlaceInfo(place, bitmap)
-
-        marker.showInfoWindow()
-    }
-
     private fun handleInfoWindowClick(marker: Marker) {
         when(marker.tag) {
 
@@ -353,11 +201,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, DrawerItemListAdap
     }
 
     private fun searchAtCurrentLocation() {
-        val bounds = RectangularBounds.newInstance(map.projection.visibleRegion.latLngBounds)
 
         try {
             val intent = Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, placeFields)
-                .setLocationBias(bounds)
+                .setLocationBias(mapManager.rectangularBounds())
                 .build(this)
             startActivityForResult(intent, REQUEST_AUTOCOMPLETE)
         } catch (error: GooglePlayServicesRepairableException) {
@@ -376,8 +223,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, DrawerItemListAdap
             REQUEST_AUTOCOMPLETE -> {
                 if (resultCode == Activity.RESULT_OK && data != null) {
                     val place = Autocomplete.getPlaceFromIntent(data)
-                    place.latLng?.let { zoomLocation(it.latitude, it.longitude) }
-                    displayPoiDetails(place)
+                    mapManager.displaySearchResults(place)
                 }
             }
         }
